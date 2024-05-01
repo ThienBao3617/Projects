78e182ada3107d8088388f8d6249a8cf256c7313
ThienBao <nltb12042003@gmail.com>
2024-05-02 01:00:48 +0700

    update project

--- Uni/create.py
+++ Uni/create.py
@@ -4,10 +4,9 @@ import time
 import firebase_admin
 from firebase_admin import db
 
-cred_obj = firebase_admin.credentials.Certificate(
-    r'D:\Github\Projects\share\qruserdatabase-firebase-adminsdk-4iw0k-517a8db948.json')
+cred_obj = firebase_admin.credentials.Certificate(add your working path)
 default_app = firebase_admin.initialize_app(cred_obj, {
-    'databaseURL': "https://qruserdatabase-default-rtdb.asia-southeast1.firebasedatabase.app/"
+   add your database url path
 })
 
 
--- Uni/generator.py
+++ Uni/generator.py
@@ -6,9 +6,9 @@ import time
 
 # Initialize Firebase
 cred_obj = firebase_admin.credentials.Certificate(
-    r'D:\Github\Projects\share\qruserdatabase-firebase-adminsdk-4iw0k-517a8db948.json')
+    add your working path)
 default_app = firebase_admin.initialize_app(cred_obj, {
-    'databaseURL': "https://qruserdatabase-default-rtdb.asia-southeast1.firebasedatabase.app/"
+    add your database url path
 })
 
 # Get the ID from your Firebase database
